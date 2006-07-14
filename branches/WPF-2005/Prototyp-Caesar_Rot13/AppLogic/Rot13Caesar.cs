e0c16afd5adacfdd14ed3c967b2ad01bc58a7b9a
beckmann <beckmann@fc492f50-1e16-0410-b377-822cd13b4496>
2006-07-14 14:05:13 +0000

    Restructure

--- branches/WPF-2005/Prototyp-Caesar_Rot13/AppLogic/Rot13Caesar.cs
+++ branches/WPF-2005/Prototyp-Caesar_Rot13/AppLogic/Rot13Caesar.cs
@@ -2,51 +2,109 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Security.Cryptography;
-using Interfaces;
 
-namespace AppLogic
+
+
+namespace CrypTool.AppLogic
 {
-    public sealed class Rot13Caesar:SymmetricAlgorithm, IEncryptionClassic
+    public sealed class Rot13Caesar:SymmetricAlgorithm, ICryptoTransform
     {
+        public Rot13Caesar()
+        {
+            this.LegalKeySizesValue = new KeySizes[] { new KeySizes(16, 16, 0) };
+
+            this.KeySize = 16; // in bits
+
+            this.LegalBlockSizesValue = new KeySizes[] { new KeySizes(16, 16, 0) }; // this is in bits - typical of MS - always 16 bytes
+
+            this.BlockSize = 16; // set this to 16 bytes we cannot have any other value
+        }
+
         public override ICryptoTransform CreateDecryptor(byte[] rgbKey, byte[] rgbIV)
         {
-            throw new Exception("The method or operation is not implemented.");
+            Key = rgbKey;
+            IV = rgbIV;
+            return this;
         }
 
         public override ICryptoTransform CreateEncryptor(byte[] rgbKey, byte[] rgbIV)
         {
-            throw new Exception("The method or operation is not implemented.");
+            Key = rgbKey;
+            IV = rgbIV;
+            return this;
         }
 
         public override void GenerateIV()
         {
-            throw new Exception("The method or operation is not implemented.");
+            //Isn't used in Rot13Caesar
+            IVValue = new byte[BlockSize/8];
+            
+
         }
 
         public override void GenerateKey()
         {
-            throw new Exception("The method or operation is not implemented.");
+            //Standard for Rot13Caesar is "M"
+            KeyValue = new byte[KeySize/8];
+            Key = Encoding.Unicode.GetBytes("M");
         }
 
-        #region IEncryptionClassic Members
 
-        void IEncryptionClassic.sampleMethodEspeciallyForClassicAlgorithms()
+        #region ICryptoTransform Members
+
+        private bool canReuseTransform=false;
+        public bool CanReuseTransform
         {
-            throw new Exception("The method or operation is not implemented.");
+            get { return canReuseTransform; }
         }
 
-        #endregion
-
-        #region IEncryptionAlgorithm Members
-
-        byte[] IEncryptionAlgorithm.encrypt(byte[] data)
+        private bool canTransformMultipleBlocks = false;
+        public bool CanTransformMultipleBlocks
         {
-            throw new Exception("The method or operation is not implemented.");
+            get { return canTransformMultipleBlocks; }
         }
 
-        byte[] IEncryptionAlgorithm.decrypt(byte[] data)
+        public int InputBlockSize
         {
-            throw new Exception("The method or operation is not implemented.");
+            get { return BlockSize / 8; }
+        }
+
+        public int OutputBlockSize
+        {
+            get { return BlockSize / 8; }
+        }
+
+        public int TransformBlock(byte[] inputBuffer, int inputOffset, int inputCount, byte[] outputBuffer, int outputOffset)
+        {
+            int bytesWritten = 0;
+            for (int i = inputOffset; i < inputOffset + inputCount; i=i+2)
+            {
+                //just as an example
+                outputBuffer[i+outputOffset-inputOffset] = (byte)(inputBuffer[i] + Key[0] - 65);
+                if (outputBuffer[i + outputOffset - inputOffset] > 90)
+                {
+                    outputBuffer[i + outputOffset - inputOffset] = (byte) (outputBuffer[i + outputOffset - inputOffset] - 90+65);
+                }
+                bytesWritten++;
+            }
+
+            return bytesWritten * 2;
+            
+        }
+
+        public byte[] TransformFinalBlock(byte[] inputBuffer, int inputOffset, int inputCount)
+        {
+            for (int i = inputOffset; i < inputOffset + inputCount; i = i + 2)
+            {
+                //just as an example
+                inputBuffer[i] = (byte)(inputBuffer[i] + Key[0] - 65);
+                if (inputBuffer[i] > 90)
+                {
+                    inputBuffer[i] = (byte)(inputBuffer[i] - 90+65);
+                }
+            }
+            return inputBuffer;
+
         }
 
         #endregion
--- branches/WPF-2005/Prototyp-Caesar_Rot13/AppLogic/TextOptions.cs
+++ branches/WPF-2005/Prototyp-Caesar_Rot13/AppLogic/TextOptions.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace AppLogic
+namespace CrypTool.AppLogic
 {
     public static class TextOptions
     {
