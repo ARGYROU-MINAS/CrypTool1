73bf60332ce51e8841afbba8d9ca48e774d904a1
przybylski <przybylski@fc492f50-1e16-0410-b377-822cd13b4496>
2007-01-10 23:40:27 +0000

    Rebuild branch for Alpha1

---
+++ branches/CrypTool2.0/Prototyp-Caesar_Rot13/Interface/IEncryptionClassic.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    public interface IEncryptionClassic:IEncryptionAlgorithm
+    {
+        void sampleMethodEspeciallyForClassicAlgorithms();
+
+    }
+}
