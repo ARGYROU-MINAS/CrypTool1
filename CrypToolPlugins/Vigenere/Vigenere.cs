919f4bd75466328f8c047effd88edcf4bbf02d44
przybylski <przybylski@fc492f50-1e16-0410-b377-822cd13b4496>
2007-10-09 20:13:46 +0000

    added new plugins: caesar, rsa, substitution, vigenere, xor

---
+++ CrypToolPlugins/Vigenere/Vigenere.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CrypTool.PluginBase;
+
+namespace CrypTool.Vigenere
+{
+    public class Vigenere
+    {
+        /// <summary>
+        /// Vigenere Encryption
+        /// </summary>
+        /// <param name="inputData">The input data to be encrypted</param>
+        /// <param name="key">The key which the encryption uses</param>
+        /// <param name="alphLength">The length of the alphabet</param>
+        /// <returns>The encrpyted data as an int array</returns>
+        public int[] Encrypt(int[] inputData, int[] key, int alphLength)
+        {
+            int[] outputData = new int[inputData.Length];
+
+            for (int i = 0; i < inputData.Length; i++)
+            {
+                outputData[i] = (inputData[i] + key[i % key.Length]) & alphLength;
+            }
+
+            return outputData;
+        }
+
+        /// <summary>
+        /// Vigenere Decryption
+        /// </summary>
+        /// <param name="inputData">The input data to be decrypted</param>
+        /// <param name="key">The key which the encryption uses</param>
+        /// <param name="alphLength">The lenght of the alphabet</param>
+        /// <returns>The decrypted data as an int array</returns>
+        public int[] Decrypt(int[] inputData, int[] key, int alphLength)
+        {
+            int[] outputData = new int[inputData.Length];
+            int newCharValue;
+
+            for (int i = 0; i < inputData.Length; i++)
+            {
+                newCharValue = inputData[i] - key[i % key.Length];
+
+                if (newCharValue < 0)
+                    newCharValue += alphLength;
+
+                outputData[i] = newCharValue;
+            }
+
+            return outputData;
+        }
+    }
+}
---
+++ CrypToolPlugins/XOR/Xor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CrypTool.PluginBase;
+
+namespace CrypTool.XOR
+{
+    public class Xor
+    {
+
+        /// <summary>
+        /// Xor Encryption
+        /// </summary>
+        /// <param name="inputData">The input data to be encrypted</param>
+        /// <param name="key">Key which the encryption uses</param>
+        /// <returns>The encrypted data as an int array</returns>
+        public int[] Encrypt(int[] inputData, int[] key)
+        {
+            int[] outputData = new int[inputData.Length];
+
+            for (int i = 0; i < inputData.Length; i++)
+            {
+                outputData[i] = inputData[i] ^ key[i % key.Length];
+            }
+
+            return outputData;
+        }
+
+        /// <summary>
+        /// Xor Decryption
+        /// </summary>
+        /// <param name="inputData">The input data to be decrypted</param>
+        /// <param name="key">Key which the decryption uses</param>
+        /// <returns>The decrypted data as an int array</returns>
+        public int[] Decrypt(int[] inputData, int[] key)
+        {
+            int[] outputData = new int[inputData.Length];
+
+            for (int i = 0; i < inputData.Length; i++)
+            {
+                outputData[i] = inputData[i] ^ key[i % key.Length];
+            }
+
+            return outputData;
+        }
+    }
+}
