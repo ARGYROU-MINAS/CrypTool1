60639a18f611e1faeb6e446677da4945bb7187cf
przybylski <przybylski@fc492f50-1e16-0410-b377-822cd13b4496>
2007-10-10 20:12:30 +0000

    added new plugins, adfgvx, playfair

---
+++ branches/CrypTool2.0/CrypToolPlugins/ADFGVX/ADFGVX.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CrypTool.PluginBase;
+using System.Collections;
+
+namespace CrypTool.ADFGVX
+{
+    public class ADFGVX
+    {
+        /// <summary>
+        /// ADFGVX encryptor
+        /// </summary>
+        /// <param name="inputData">The input data to be encrypted</param>
+        /// <param name="key">Key used for encryption</param>
+        /// <param name="alphabet">alphabet used for encryption</param>
+        /// <param name="inputNoNonAlphaChar">char array containing no nonalphabetical characters</param>
+        /// <param name="key2"></param>
+        /// <returns>The encrypted data as an int array</returns>
+        public int[] Encrypt(int[] inputData, int[] key, int[] alphabet, int[] key2)
+        {
+            int[] alphCipher = getAlphCipher(removeDuplicateChars(key), alphabet);
+            char[] outputData = new char[inputData.Length * 2];
+
+            int y = 0;
+            for (int i = 0; i < inputData.Length && y < outputData.Length + 1; i++)
+            {
+                for (int j = 0; j < alphCipher.Length; j++)
+                {
+                    if (alphCipher[j] == inputData[i])
+                    {
+                        int line = j / 6;
+                        int column = j % 6;
+                        String pair = "";
+                        switch (line)
+                        {
+                            case 0:
+                                pair = "A";
+                                break;
+                            case 1:
+                                pair = "D";
+                                break;
+                            case 2:
+                                pair = "F";
+                                break;
+                            case 3:
+                                pair = "G";
+                                break;
+                            case 4:
+                                pair = "V";
+                                break;
+                            case 5:
+                                pair = "X";
+                                break;
+                        }
+                        switch (column)
+                        {
+                            case 0:
+                                pair += "A";
+                                break;
+                            case 1:
+                                pair += "D";
+                                break;
+                            case 2:
+                                pair += "F";
+                                break;
+                            case 3:
+                                pair += "G";
+                                break;
+                            case 4:
+                                pair += "V";
+                                break;
+                            case 5:
+                                pair += "X";
+                                break;
+                        }
+                        char[] tempPair = pair.ToCharArray();
+                        outputData[y] = tempPair[0];
+                        y++;
+                        outputData[y] = tempPair[1];
+                        y++;
+                    }
+                }
+            }
+            int[] order = getOrder(key2);
+            int count = 0;
+            char[] charOutputData = new char[outputData.Length];
+            for (int i = 0; i < order.Length; i++)
+            {
+                int tempOrder = order[i];
+                for (int j = tempOrder; j < outputData.Length; j=j + order.Length)
+                {
+                    charOutputData[count] = outputData[j];
+                    count++;
+                }
+            }
+
+            int[] dummy = null;
+            return dummy; // charOutputData, missing Alphabet
+        }
+
+        /// <summary>
+        /// ADFGVX decryptor
+        /// </summary>
+        /// <param name="inputData">The input data to be decryted</param>
+        /// <param name="key">Key used for decryption</param>
+        /// <param name="alphabet">alphabet used for decryption</param>
+        /// <param name="inputNoNonAlphaChar">char array containing no nonalphabetical characters</param>
+        /// <param name="key2"></param>
+        /// <returns>The decrypted data as an int array</returns>
+        public int[] Decrypt(int[] inputData, int[] key, int[] alphabet, char[] inputNoNonAlphaChar, int[] key2)
+        {
+            int[] alphCipher = getAlphCipher(removeDuplicateChars(key), alphabet);
+            int[] ouputData = new int[inputNoNonAlphaChar.Length / 2];
+            int[] order = getOrder(key2);
+            int count = 0;
+            char[] charOutputData = new char[inputNoNonAlphaChar.Length];
+
+            for (int i = 0; i < order.Length; i++)
+            {
+                int ord = order[i];
+                for (int j = ord; j < inputNoNonAlphaChar.Length; j=j + order.Length)
+                {
+                    charOutputData[j] = inputNoNonAlphaChar[count];
+                    count++;
+                }
+            }
+
+            int y = 0;
+            for (int i = 0; i < charOutputData.Length; i++)
+            {
+                char[] pair = new char[2];
+                int line = 0;
+                int column = 0;
+                pair[0] = charOutputData[i];
+                i++;
+                pair[1] = charOutputData[i];
+
+                switch (pair[0])
+                {
+                    case 'A':
+                        line = 0;
+                        break;
+                    case 'D':
+                        line = 1;
+                        break;
+                    case 'F':
+                        line = 2;
+                        break;
+                    case 'G':
+                        line = 3;
+                        break;
+                    case 'V':
+                        line = 4;
+                        break;
+                    case 'X':
+                        line = 5;
+                        break;
+                }
+                switch (pair[1])
+                {
+                    case 'A':
+                        column = 0;
+                        break;
+                    case 'D':
+                        column = 1;
+                        break;
+                    case 'F':
+                        column = 2;
+                        break;
+                    case 'G':
+                        column = 3;
+                        break;
+                    case 'V':
+                        column = 4;
+                        break;
+                    case 'X':
+                        column = 5;
+                        break;
+                }
+                int ch = line * 6 + column;
+                ouputData[y] = alphCipher[ch];
+                y++;
+            }
+            return ouputData;
+        }
+
+        /// <summary>
+        /// Generate and initialize the private data
+        /// </summary>
+        /// <param name="key2">The key to be used for the permutation</param>
+        /// <returns>an int array which contains the read-order of the columns</returns>
+        private int[] getOrder(int[] key2)
+        {
+            int[] mP = new int[key2.Length];
+            int[] mIP = new int[key2.Length];
+
+            for (int i = 0; i < key2.Length; i++)
+            {
+                mP[i] = 0;
+                for (int j = 0; j < key2.Length; j++)
+                {
+                    if (i != j)
+                    {
+                        if (key2[i] > key2[j])
+                        {
+                            mP[i]++;
+                        }
+                        else if ((key2[i] == key2[j]) && (i < j))
+                        {
+                            mP[i]++;
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < key2.Length; i++)
+            {
+                mIP[mP[i]] = i;
+            }
+            return mIP;
+        }
+
+        /// <summary>
+        /// Build alphabet cipher
+        /// </summary>
+        /// <param name="key">used key</param>
+        /// <param name="alphabet">The plain alphabet</param>
+        /// <returns>Alphabet for en-/decryption as an int array</returns>
+        private int[] getAlphCipher(int[] key, int[] alphabet)
+        {
+            int count = 0;
+            bool found;
+            int[] alphCipher = new int[alphabet.Length];
+
+            for (int i = 0; i < key.Length; i++)
+            {
+                alphCipher[i] = key[i];
+            }
+
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                found = false;
+                for (int j = 0; j < key.Length && !found; j++)
+                {
+                    if (alphabet[i] == key[j])
+                    {
+                        found = true;
+                        count++;
+                    }
+                }
+                if (!found)
+                {
+                    alphCipher[i + key.Length - count] = alphabet[i];
+                }
+            }
+
+            return alphCipher;
+        }
+
+        /// <summary>
+        /// Remove duplicate characters from the key array
+        /// </summary>
+        /// <param name="key">The key array</param>
+        /// <returns>The key array without duplicate characters</returns>
+        private int[] removeDuplicateChars(int[] key)
+        {
+            bool found;
+            int[] newKey;
+            ArrayList alKey = new ArrayList();
+
+            for (int i = 0; i < key.Length; i++)
+            {
+                found = false;
+                for (int j = 0; j < alKey.Count; j++)
+                {
+                    if (key[i] == (int)alKey[j])
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    alKey.Add(key[i]);
+                }
+            }
+
+            newKey = new int[alKey.Count];
+            for (int i = 0; i < alKey.Count; i++)
+            {
+                newKey[i] = (int)alKey[i];
+            }
+
+            return newKey;
+        }
+    }
+}
---
+++ branches/CrypTool2.0/CrypToolPlugins/Playfair/Playfair.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CrypTool.PluginBase;
+using System.Collections;
+
+namespace CrypTool.Playfair
+{
+    public class Playfair
+    {
+        /// <summary>
+        /// Playfair encryption
+        /// </summary>
+        /// <param name="inputData">The input data to be encrypted</param>
+        /// <param name="key">Key used for the encryption</param>
+        /// <param name="inputNoNonAlphChar"></param>
+        /// <param name="alphabet"></param>
+        /// <param name="nullChar"></param>
+        /// <param name="keyChars"></param>
+        /// <returns></returns>
+        public int[] Encrypt(int[] inputData, int[] key, char[] inputNoNonAlphChar,int[] alphabet, char nullChar, char[] keyChars)
+        {
+            int pairs = 0;
+            for (int i = 0; i < inputData.Length; i++)
+            {
+                if (char.IsLetter(keyChars[i]))
+                {
+                    inputData[i] = char.ToUpper(keyChars[i]);
+                }
+                if (inputData[i] == 'J')
+                {
+                    inputData[i] = 'I';
+                }
+                if (inputData[i] == 'Ä')
+                {
+                    inputData[i] = 'A';
+                }
+                if (inputData[i] == 'Ö')
+                {
+                    inputData[i] = 'O';
+                }
+                if (inputData[i] == 'Ü')
+                {
+                    inputData[i] = 'U';
+                }
+            }
+            for (int i = 0; i < inputNoNonAlphChar.Length; i++)
+            {
+                if (i < inputNoNonAlphChar.Length - 1)
+                {
+                    if (inputNoNonAlphChar[i] == inputNoNonAlphChar[i + 1])
+                    {
+                        pairs++;
+                    }
+                }
+            }
+
+            char[] inputEven;
+            if ((inputNoNonAlphChar.Length + pairs) % 2 != 0)
+            {
+                inputEven = new char[inputNoNonAlphChar.Length + 1 + pairs];
+                int counter = 0;
+                for (int i = 0; i < inputNoNonAlphChar.Length; i++)
+                {
+                    if (i < inputNoNonAlphChar.Length - 1)
+                    {
+                        if (inputNoNonAlphChar[i] == inputNoNonAlphChar[i + 1])
+                        {
+                            inputEven[counter] = inputNoNonAlphChar[i];
+                            counter++;
+                            inputEven[counter] = nullChar;
+                            counter++;
+                        }
+                        else
+                        {
+                            inputEven[counter] = inputNoNonAlphChar[i];
+                            counter++;
+                        }
+                    }
+                    else
+                    {
+                        inputEven[counter] = inputNoNonAlphChar[i];
+                        counter++;
+                    }
+                }
+                inputEven[inputEven.Length - 1] = nullChar;
+            }
+            else
+            {
+                inputEven = new char[inputNoNonAlphChar.Length + pairs];
+                int counter = 0;
+                for (int i = 0; i < inputNoNonAlphChar.Length; i++)
+                {
+                    if (i < inputNoNonAlphChar.Length - 1)
+                    {
+                        if (inputNoNonAlphChar[i] == inputNoNonAlphChar[i + 1])
+                        {
+                            inputEven[counter] = inputNoNonAlphChar[i];
+                            counter++;
+                            inputEven[counter] = nullChar;
+                            counter++;
+                        }
+                        else
+                        {
+                            inputEven[counter] = inputNoNonAlphChar[i];
+                            counter++;
+                        }
+                    }
+                    else
+                    {
+                        inputEven[counter] = inputNoNonAlphChar[i];
+                        counter++;
+                    }
+                }
+            }
+
+            int[] alphCipher = getAlphCipher(removeDuplicateChars(key), alphabet);
+            int[] inputInt = null; //todo convert char array inputEven to int array
+            int[] outputData = new int[inputInt.Length];
+            for (int i = 0; i < inputInt.Length - 1; i++)
+            {
+                int ch1 = inputInt[i];
+                i++;
+                int ch2 = inputInt[i];
+                int indexCh1 = getIndex(ch1, alphCipher);
+                int indexCh2 = getIndex(ch2, alphCipher);
+                int rowCh1 = -1;
+                int rowCh2 = -2;
+                int colCh1 = -3;
+                int colCh2 = -4;
+                int newCh1;
+                int newCh2;
+
+                for (int j = 0; j < alphCipher.Length; j++)
+                {
+                    if (alphCipher[j] == ch1)
+                    {
+                        rowCh1 = j / 5;
+                        colCh1 = j % 5;
+                    }
+                    if (alphCipher[j] == ch2)
+                    {
+                        rowCh2 = j / 5;
+                        colCh2 = j % 5;
+                    }
+                }
+                if (rowCh1 == rowCh2)
+                {
+                    newCh1 = getRightNeighbour(indexCh1, alphCipher);
+                    newCh2 = getRightNeighbour(indexCh2, alphCipher);
+                }
+                else if (colCh1 == colCh2)
+                {
+                    newCh1 = getLowerNeighbour(indexCh1, alphCipher);
+                    newCh2 = getLowerNeighbour(indexCh2, alphCipher);
+                }
+                else
+                {
+                    newCh1 = getSubstitute(rowCh1, colCh2, alphCipher);
+                    newCh2 = getSubstitute(rowCh2, colCh1, alphCipher);
+                }
+                outputData[i - 1] = newCh1;
+                outputData[i] = newCh2;
+            }
+            return outputData;
+        }
+
+        public int[] Decrypt(int[] inputData, int[] key, char[] inputNoNonAlphChar, int[] alphabet, char nullChar, char[] keyChars)
+        {
+            int[] alphCipher = getAlphCipher(removeDuplicateChars(key), alphabet);
+            int[] outputData = new int[inputData.Length];
+            for (int i = 0; i < inputData.Length - 1; i++)
+            {
+                int ch1 = inputData[i];
+                i++;
+                int ch2 = inputData[i];
+                int indexCh1 = getIndex(ch1, alphCipher);
+                int indexCh2 = getIndex(ch2, alphCipher);
+                int rowCh1 = -1;
+                int rowCh2 = -2;
+                int colCh1 = -3;
+                int colCh2 = -4;
+                int newCh1;
+                int newCh2;
+
+                for (int j = 0; j < alphCipher.Length; j++)
+                {
+                    if (alphCipher[j] == ch1)
+                    {
+                        rowCh1 = j / 5;
+                        colCh1 = j % 5;
+                    }
+                    if (alphCipher[j] == ch2)
+                    {
+                        rowCh2 = j / 5;
+                        colCh2 = j % 5;
+                    }
+                }
+                if (rowCh1 == rowCh2)
+                {
+                    newCh1 = getLeftNeighbour(indexCh1, alphCipher);
+                    newCh2 = getLeftNeighbour(indexCh2, alphCipher);
+                }
+                else if (colCh1 == colCh2)
+                {
+                    newCh1 = getUpperNeighbour(indexCh1, alphCipher);
+                    newCh2 = getUpperNeighbour(indexCh2, alphCipher);
+                }
+                else
+                {
+                    newCh1 = getSubstitute(rowCh1, colCh2, alphCipher);
+                    newCh2 = getSubstitute(rowCh2, colCh1, alphCipher);
+                }
+                outputData[i - 1] = newCh1;
+                outputData[i] = newCh2;
+            }
+            return outputData;
+        }
+
+        /// <summary>
+        /// Build alphabet cipher
+        /// </summary>
+        /// <param name="key">used key</param>
+        /// <param name="alphabet">The plain alphabet</param>
+        /// <returns>Alphabet for en-/decryption as an int array</returns>
+        private int[] getAlphCipher(int[] key, int[] alphabet)
+        {
+            int count = 0;
+            bool found;
+            int[] alphCipher = new int[alphabet.Length];
+
+            for (int i = 0; i < key.Length; i++)
+            {
+                alphCipher[i] = key[i];
+            }
+
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                found = false;
+                for (int j = 0; j < key.Length && !found; j++)
+                {
+                    if (alphabet[i] == key[j])
+                    {
+                        found = true;
+                        count++;
+                    }
+                }
+                if (!found)
+                {
+                    alphCipher[i + key.Length - count] = alphabet[i];
+                }
+            }
+
+            return alphCipher;
+        }
+
+        /// <summary>
+        /// Remove duplicate characters from the key array
+        /// </summary>
+        /// <param name="key">The key array</param>
+        /// <returns>The key array without duplicate characters</returns>
+        private int[] removeDuplicateChars(int[] key)
+        {
+            bool found;
+            int[] newKey;
+            ArrayList alKey = new ArrayList();
+
+            for (int i = 0; i < key.Length; i++)
+            {
+                found = false;
+                for (int j = 0; j < alKey.Count; j++)
+                {
+                    if (key[i] == (int)alKey[j])
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    alKey.Add(key[i]);
+                }
+            }
+
+            newKey = new int[alKey.Count];
+            for (int i = 0; i < alKey.Count; i++)
+            {
+                newKey[i] = (int)alKey[i];
+            }
+
+            return newKey;
+        }
+
+        private int getIndex(int ch, int[] alphCipher)
+        {
+            for (int i = 0; i < alphCipher.Length; i++)
+            {
+                if (ch == alphCipher[i])
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        private int getRightNeighbour(int index, int[] alphCipher)
+        {
+            if (index % 5 < 4)
+            {
+                return alphCipher[index + 1];
+            }
+            else
+            {
+                if (index % 5 == 4)
+                {
+                    return alphCipher[index - 4];
+                }
+            }
+            return -1;
+        }
+
+        private int getLowerNeighbour(int index, int[] alphCipher)
+        {
+            if (index + 5 < alphCipher.Length)
+            {
+                return alphCipher[index + 5];
+            }
+            else
+            {
+                return alphCipher[(index + 5) % alphCipher.Length];
+            }
+        }
+
+        private int getUpperNeighbour(int index, int[] alphCipher)
+        {
+            if (index < 5)
+            {
+                return alphCipher[alphCipher.Length - (5 - index)];
+            }
+            else
+            {
+                return alphCipher[index - 5];
+            }
+        }
+
+        private int getLeftNeighbour(int index, int[] alphCipher)
+        {
+            if (index % 5 > 0)
+            {
+                return alphCipher[index - 1];
+            }
+            else
+            {
+                return alphCipher[index + 4];
+            }
+        }
+
+        private int getSubstitute(int row, int col, int[] alphCipher)
+        {
+            return alphCipher[5 * (row) + col];
+        }
+    }
+}
