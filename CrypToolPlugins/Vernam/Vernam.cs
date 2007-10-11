6f622250403850e3931455ef3f3a09c534c9abec
przybylski <przybylski@fc492f50-1e16-0410-b377-822cd13b4496>
2007-10-11 17:00:23 +0000

    added new plugins: started with hill, transposition, vernam, playfair

---
+++ CrypToolPlugins/Vernam/Vernam.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CrypTool.PluginBase;
+
+namespace CrypTool.Vernam
+{
+    public class Vernam
+    {
+        public int[] Encrypt(int[] inputData, int[] key)
+        {
+            int[] outputData = new int[inputData.Length];
+            key = refillKey(key,inputData.Length);
+
+            for (int i = 0; i < inputData.Length; i++)
+			{
+                outputData[i] = inputData[i] ^ key[i];
+			}
+
+            return outputData;
+        }
+
+        public int[] Decrypt(int[] inputData, int[]key)
+        {
+            int[] outputData = new int[inputData.Length];
+            key = refillKey(key,inputData.Length);
+
+            for (int i = 0; i < inputData.Length; i++)
+			{
+                outputData[i] = inputData[i] ^ key[i];
+			}
+
+            return outputData;
+        }
+
+        private int[] refillKey(int[] key, int inputDataLength)
+        {
+            int[] fullKey = new int[inputDataLength];
+
+            for (int i = 0; i < key.Length; i++)
+			{
+                fullKey[i] = key[i];
+			}
+
+            int pos = 1;
+            for (int i = key.Length; i < inputDataLength; i++)
+			{
+                fullKey[i] = key[pos];
+                pos++;
+			}
+            return fullKey;
+        }
+    }
+}
